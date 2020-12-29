103508010c7fe5df1951c5ac969d1defcb5470ca
hugorebel1998 <hogorebel1998@gmail.com>
2020-12-29 00:47:38 -0600

    Consumiendo la API de paypal

--- app/Services/PayPalService.php
+++ app/Services/PayPalService.php
@@ -1,45 +1,46 @@
-<?php
-
+<?php   
 namespace App\Services;
-
+ 
 use App\Traits\ConsumesExternalServices;
-use Illuminate\Support\Facades\Http;
-
-class PayPalService
+ 
+class PaypalService 
 {
-
     use ConsumesExternalServices;
 
-    protected $baseUri;
-
-    protected $clientId;
-
-    protected $clientSecret;
-
-    public function __construct()
-    {
-        $this->baseUri = config('services.paypal.base_uri');
-        $this->clientId = config('services.paypal.client_id');
-        $this->clientSecret = config('services.paypal.client_secret');
-    }
-
-    public function resolveAuthorization(&$queryParams, &$formParams, &$headers)
-    {
-        $headers['Authorization'] = $this->resolveAccessToken();
-    }
-
-    public function decodeResponse($response)
-    {
-        return json_decode($response);
-    }
-
-    public function resolveAccessToken()
-    {
-        $credentials = base64_encode("{$this->clientId}:{$this->clientSecret}");
-
-        return "Basic{$credentials}";
-    }
-
+	protected $baseUri;
+ 
+	protected $clientId;
+ 
+	protected $clientSecret;
+	
+	public function __construct()
+	{
+		# code...
+		$this->baseUri= config('services.paypal.base_uri');
+		$this->clientId= config('services.paypal.client_id');
+        $this->clientSecret= config('services.paypal.secret');
+        
+	}
+ 
+	public function resolveAuthorization(&$queryParams, &$formParams, &$headers)
+	{
+		# code...
+		$headers['Authorization'] = $this->resolveAccessToken();
+	}
+ 
+	public function decodeResponse($response)
+	{
+		# code...
+		return json_decode($response);
+	}
+	public function resolveAccessToken()
+	{
+		# code...
+		$credentials = base64_encode("{$this->clientId}:{$this->clientSecret}");
+ 
+		return "Basic {$credentials}";
+	}
+ 
     public function createOrder($value, $currency)
     {
         return $this->makeRequest(
